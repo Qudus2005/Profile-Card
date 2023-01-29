bc204f81b1c8c4dd5c64d95b710df85a7f81a98f
Hassan Qudus <114084917+Qudus2005@users.noreply.github.com>
2023-01-29 15:40:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Ask for the user name... 
+var name = prompt('What Is Your Name?')
+
+document.getElementById("name").innerHTML = name
+// Write into html div the current date and time... 
+document.getElementById("time").innerHTML = Date()
